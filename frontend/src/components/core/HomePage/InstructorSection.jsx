9e619113c03b9cde6e67252e23de27e6ce8c94f3
royabhishek2003 <kishanabhishek2003@gmail.com>
2026-02-17 23:03:36 +0530

    fix import path

--- frontend/src/components/core/HomePage/InstructorSection.jsx
+++ frontend/src/components/core/HomePage/InstructorSection.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import CTAButton from "../../../components/core/HomePage/Button";
 import { FaArrowRight } from "react-icons/fa";
 import Instructor from "../../../assets/Images/Instructor.png";
-import HighlightText from "../HomePage/HighLightText";
+import HighlightText from "./HighlightText";
 
 const InstructorSection = () => {
   return (
