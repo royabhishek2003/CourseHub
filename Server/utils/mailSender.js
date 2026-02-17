915dc69fbc713afe9346220a08f61eaf23eb3957
royabhishek2003 <kishanabhishek2003@gmail.com>
2026-02-18 00:56:16 +0530

    fix gmail smtp timeout

--- Server/utils/mailSender.js
+++ Server/utils/mailSender.js
@@ -1,28 +1,30 @@
-const nodemailer = require("nodemailer")
+const nodemailer = require("nodemailer");
 
 const mailSender = async (email, title, body) => {
   try {
-    let transporter = nodemailer.createTransport({
-      host: process.env.MAIL_HOST,
+    const transporter = nodemailer.createTransport({
+      host: "smtp.gmail.com",
+      port: 465,
+      secure: true, // REQUIRED for port 465
       auth: {
         user: process.env.MAIL_USER,
         pass: process.env.MAIL_PASS,
       },
-      secure: false,
-    })
+    });
 
-    let info = await transporter.sendMail({
-      from: `"Studynotion | CodeHelp" <${process.env.MAIL_USER}>`, // sender address
-      to: `${email}`, // list of receivers
-      subject: `${title}`, // Subject line
-      html: `${body}`, // html body
-    })
-    console.log(info.response)
-    return info
+    const info = await transporter.sendMail({
+      from: `"Studynotion | CodeHelp" <${process.env.MAIL_USER}>`,
+      to: email,
+      subject: title,
+      html: body,
+    });
+
+    console.log("Mail response:", info.response);
+    return info;
   } catch (error) {
-    console.log(error.message)
-    return error.message
+    console.log("Mail error:", error.message);
+    throw error; // important: don't silently swallow
   }
-}
+};
 
-module.exports = mailSender
+module.exports = mailSender;
