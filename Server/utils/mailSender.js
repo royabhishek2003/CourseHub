1d7668208042bfb474087d0e2d68417a084334e4
royabhishek2003 <kishanabhishek2003@gmail.com>
2026-02-18 02:13:24 +0530

    add smtp mail

--- Server/utils/mailSender.js
+++ Server/utils/mailSender.js
@@ -3,39 +3,20 @@ const nodemailer = require("nodemailer");
 const mailSender = async (email, title, body) => {
   try {
     const transporter = nodemailer.createTransport({
-      service: "gmail",
-      auth: {
-        user: process.env.MAIL_USER,
-        pass: process.env.MAIL_PASS, // Gmail App Password (not real Gmail password)
-      },
-    });
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false, // IMPORTANT
+  auth: {
+    user: process.env.MAIL_USER,
+    pass: process.env.MAIL_PASS,
+  },
+  tls: {
+    rejectUnauthorized: false,
+  },
+});
 
     const info = await transporter.sendMail({
-      from: `"Studynotion" <${process.env.MAIL_USER}>`,
-      to: email,
-      subject: title,
-      html: body,
-    });
-
-    console.log("Email sent:", info.messageId);
-    return info;
-  } catch (error) {
-    console.error("Mail error:", error);
-    throw error;
-  }
-};
-
-module.exports = mailSender;
-
-
-const { Resend } = require("resend");
-
-const resend = new Resend(process.env.RESEND_API_KEY);
-
-const mailSender = async (email, title, body) => {
-  try {
-    const info = await resend.emails.send({
-      from: "Studynotion | CodeHelp <onboarding@resend.dev>",
+      from: `"Studynotion | CodeHelp" <${process.env.MAIL_USER}>`,
       to: email,
       subject: title,
       html: body,
@@ -50,4 +31,28 @@ const mailSender = async (email, title, body) => {
 };
 
 module.exports = mailSender;
+
+
+// const { Resend } = require("resend");
+
+// const resend = new Resend(process.env.RESEND_API_KEY);
+
+// const mailSender = async (email, title, body) => {
+//   try {
+//     const info = await resend.emails.send({
+//       from: "Studynotion | CodeHelp <onboarding@resend.dev>",
+//       to: email,
+//       subject: title,
+//       html: body,
+//     });
+
+//     console.log("Full mail info:", info);
+//     return info;
+//   } catch (error) {
+//     console.log("Mail error:", error.message);
+//     throw error; // important: don't silently swallow
+//   }
+// };
+
+// module.exports = mailSender;
 
